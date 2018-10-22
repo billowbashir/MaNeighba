262d0ca5a8c0a59902451026e0e5957d64fc1ac6
billowbashir <billowbashir@gmail.com>
2018-10-22 12:16:37 +0300

    edit files

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Neighbourhood,Profile,Business
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
