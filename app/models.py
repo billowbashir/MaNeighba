2502e0f8ce9bb25bb9dbeba9d9b325ba14ba4c8a
billowbashir <billowbashir@gmail.com>
2018-10-22 09:13:51 +0300

    project urls

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Neighbourhood(models.Model):
+    name=models.CharField(max_length=60)
+    location=models.CharField(max_length=60)
+    population=models.IntegerField()
+    admin=models.ForeignKey(User,on_delete=models.CASCADE)
+
+# create_neigborhood()
+# delete_neigborhood()
+# find_neigborhood(neigborhood_id)
+# update_neighborhood()
+# update_occupants()
+
+
+class Profile(models.Model):
+    profile_pic=models.ImageField(upload_to='profile_photos/')
+    bio=models.CharField(max_length=300)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighborhood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
