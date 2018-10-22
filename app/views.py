73ecb55cce92f1c671ff417051fba354aa2903b6
billowbashir <billowbashir@gmail.com>
2018-10-22 09:22:19 +0300

    views and urls configure

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Neighbourhood,Profile
+# from .forms import NewProjectForm,NewProfileForm,RateForm
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    
+    return render(request,'index.html')
